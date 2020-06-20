aee3011de00b775cdbd0d10f6c49228050d94df2
anekobakht <a.nekobakht17439abcd@gmail.com>
2020-06-20 09:46:56 +0430

    Updates

---
+++ precache-manifest.4b0215bea247abc29c7e63f08d8c7a41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc9a2a80f8917e5fe56c0bc835f8349",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "f5b88700bf10989ebd51",
+    "url": "/Resume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "86c56d8d8cd8d9a469ce",
+    "url": "/Resume/static/css/main.163233b5.chunk.css"
+  },
+  {
+    "revision": "f5b88700bf10989ebd51",
+    "url": "/Resume/static/js/2.cc94e678.chunk.js"
+  },
+  {
+    "revision": "9258cdbe2f6d55795e5caf494bfb448e",
+    "url": "/Resume/static/js/2.cc94e678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c56d8d8cd8d9a469ce",
+    "url": "/Resume/static/js/main.df79d68e.chunk.js"
+  },
+  {
+    "revision": "f1901266f684724560d5",
+    "url": "/Resume/static/js/runtime-main.6aaeef75.js"
+  }
+]);
